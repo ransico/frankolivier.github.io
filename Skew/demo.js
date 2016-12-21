8b97dab8089abc45a9ef8726dffcc72637ad4e81
frankolivier <franko@microsoft.com>
2016-12-21 11:23:28 -0800

    Added dashed line for input canvas

--- Skew/demo.js
+++ Skew/demo.js
@@ -5,7 +5,7 @@ window.onload = main;	// Startup
 
 
 
-var renderer = new function() {
+var renderer = new function () {
 
 	var gl;                 // Handle to the context
 	var pictureprogram;     // Handle to GLSL program that draws a picture
@@ -15,65 +15,63 @@ var renderer = new function() {
 	this.drawCoordLocation;	// BUGBUG?
 
 	this.texCoordBuffer;
-    
+
 
 	this.init = function () {
 		// Get a context
-	    var canvas = document.getElementById("webglcanvas");
+		var canvas = document.getElementById("webglcanvas");
 
-	    try
-	    {
-            // use antialias:false to make lines more visible
-            var gl = this.gl = canvas.getContext("experimental-webgl", { antialias:false, preserveDrawingBuffer: true });
-	    }
-	    catch (e)
-	    {
-	        // Workaround for Firefox issue
-	    }
+		try {
+			// use antialias:false to make lines more visible
+			var gl = this.gl = canvas.getContext("experimental-webgl", { antialias: false, preserveDrawingBuffer: true });
+		}
+		catch (e) {
+			// Workaround for Firefox issue
+		}
 
 
 		// Set the viewport size
 		gl.viewportWidth = canvas.width;
 		gl.viewportHeight = canvas.height;
-	    
-	    try {
-	        var vertexshader = getShader(gl, "2d-vertex-shader");
-	        var fragmentshader = getShader(gl, "2d-fragment-shader");
 
-	        this.pictureprogram = loadProgram(gl, vertexshader, fragmentshader);
-	        gl.useProgram(this.pictureprogram);
+		try {
+			var vertexshader = getShader(gl, "2d-vertex-shader");
+			var fragmentshader = getShader(gl, "2d-fragment-shader");
+
+			this.pictureprogram = loadProgram(gl, vertexshader, fragmentshader);
+			gl.useProgram(this.pictureprogram);
 
 
+
+
+
+			// look up where the vertex data needs to go.
+			this.texCoordLocation = gl.getAttribLocation(this.pictureprogram, "a_texCoord");
+
+
+			// provide texture coordinates for the rectangle.
+			this.texCoordBuffer = gl.createBuffer();
+			gl.bindBuffer(gl.ARRAY_BUFFER, this.texCoordBuffer);
+
+			var c = new Float32Array(12); //2 numbers per coord; three coords per triangle; 2 triagles per square; resolution * resolution squares
+
+			var i = 0;
+
+			/*
+						var ptlx = 0.055;
+						var ptly = 0.86;
 			
+						var ptrx = 0.79;
+						var ptry = 0.92;
+			
+						var pbrx = 0.85;
+						var pbry = 0.26;
+			
+						var pblx = 0.15;
+						var pbly = 0.1;
+			*/
 
-
-	        // look up where the vertex data needs to go.
-	        this.texCoordLocation = gl.getAttribLocation(this.pictureprogram, "a_texCoord");
-
-
-	        // provide texture coordinates for the rectangle.
-	        this.texCoordBuffer = gl.createBuffer();
-	        gl.bindBuffer(gl.ARRAY_BUFFER, this.texCoordBuffer);
-
-	        var c = new Float32Array(12); //2 numbers per coord; three coords per triangle; 2 triagles per square; resolution * resolution squares
-
-	        var i = 0;
-
-/*
-            var ptlx = 0.055;
-			var ptly = 0.86;
-
-			var ptrx = 0.79;
-			var ptry = 0.92;
-
-			var pbrx = 0.85;
-			var pbry = 0.26;
-
-            var pblx = 0.15;
-			var pbly = 0.1;
-*/
-
-            var ptlx = 0;
+			var ptlx = 0;
 			var ptly = 1;
 
 			var ptrx = 1;
@@ -82,7 +80,7 @@ var renderer = new function() {
 			var pbrx = 1;
 			var pbry = 0;
 
-            var pblx = 0;
+			var pblx = 0;
 			var pbly = 0;
 
 			c[i++] = pblx; //
@@ -103,42 +101,42 @@ var renderer = new function() {
 			c[i++] = ptrx; //
 			c[i++] = ptry;
 
-	        gl.bufferData(gl.ARRAY_BUFFER, c, gl.STATIC_DRAW);
-	        
-	        gl.vertexAttribPointer(this.texCoordLocation, 2, gl.FLOAT, false, 0, 0);
+			gl.bufferData(gl.ARRAY_BUFFER, c, gl.STATIC_DRAW);
 
-	        gl.enableVertexAttribArray(this.texCoordLocation);
+			gl.vertexAttribPointer(this.texCoordLocation, 2, gl.FLOAT, false, 0, 0);
 
-	        // Set up uniforms
+			gl.enableVertexAttribArray(this.texCoordLocation);
 
-	        // Set the texture to use
-	        this.pictureprogram.u_image = gl.getUniformLocation(this.pictureprogram, "u_image");
-	        gl.uniform1i(this.pictureprogram.u_image, 0);
+			// Set up uniforms
+
+			// Set the texture to use
+			this.pictureprogram.u_image = gl.getUniformLocation(this.pictureprogram, "u_image");
+			gl.uniform1i(this.pictureprogram.u_image, 0);
 
 			this.pictureprogram.topLeft = gl.getUniformLocation(this.pictureprogram, "topLeft");
 			this.pictureprogram.topRight = gl.getUniformLocation(this.pictureprogram, "topRight");
 			this.pictureprogram.bottomRight = gl.getUniformLocation(this.pictureprogram, "bottomRight");
 			this.pictureprogram.bottomLeft = gl.getUniformLocation(this.pictureprogram, "bottomLeft");
 
-/*
+			/*
+						
+						
 			
+						
+			*/
+			/*
+						//var topLeft = new Point(0,1);
+						var topLeft = new Point(0.06, 0.85);
 			
-
+						//var topRight = new Point(1,1);
+						var topRight = new Point(0.79, 0.92);
 			
-*/
-/*
-			//var topLeft = new Point(0,1);
-			var topLeft = new Point(0.06, 0.85);
-
-			//var topRight = new Point(1,1);
-			var topRight = new Point(0.79, 0.92);
-
-			//var bottomRight = new Point(1,0);
-			var bottomRight = new Point(0.84,0.26);
-
-			//var bottomLeft = new Point(0,0);
-			var bottomLeft = new Point(0.15,0.1);
-*/
+						//var bottomRight = new Point(1,0);
+						var bottomRight = new Point(0.84,0.26);
+			
+						//var bottomLeft = new Point(0,0);
+						var bottomLeft = new Point(0.15,0.1);
+			*/
 			//var topLeft = new Point(0,1);
 			var topLeft = new Point(0.095, 1);
 
@@ -146,42 +144,46 @@ var renderer = new function() {
 			var topRight = new Point(0.645, 1);
 
 			//var bottomRight = new Point(1,0);
-			var bottomRight = new Point(0.631,0);
+			var bottomRight = new Point(0.631, 0);
 
 			//var bottomLeft = new Point(0,0);
-			var bottomLeft = new Point(0.046,0);
+			var bottomLeft = new Point(0.046, 0);
 
-		    gl.uniform2f(this.pictureprogram.topLeft, topLeft.x, topLeft.y);
+			gl.uniform2f(this.pictureprogram.topLeft, topLeft.x, topLeft.y);
 			gl.uniform2f(this.pictureprogram.topRight, topRight.x, topRight.y);
 			gl.uniform2f(this.pictureprogram.bottomRight, bottomRight.x, bottomRight.y);
 			gl.uniform2f(this.pictureprogram.bottomLeft, bottomLeft.x, bottomLeft.y);
 
-	    }
+		}
 
-	    catch (e) {
-	        log('shader fail');
-	        return;
-	    }
-        
-        
-	    this.loadImage();
+		catch (e) {
+			log('shader fail');
+			return;
+		}
+
+
+		this.loadImage();
 	}
+
+	this.inputImage = "";
+	this.canvas = document.getElementById("inputcanvas");
+	this.ctx = this.canvas.getContext("2d");
 
 	this.loadImage = function () {
 
-		var image = new Image();
-		
-		image.onload = function () {
-			renderer.loadImage2(image);
+		this.inputImage = new Image();
+
+		this.inputImage.onload = function () {
+			renderer.loadImage2(this.inputImage);
 		}
 
-		image.src = "photos/fo.jpg";
+		this.inputImage.src = "photos/fo.jpg";
 
 	}
 
 	this.loadImageX = function (dataURL) {
 		var image = new Image();
-		
+
 		image.onload = function () {
 			renderer.loadImage2(image);
 		}
@@ -189,33 +191,38 @@ var renderer = new function() {
 		image.src = dataURL;
 	}
 
-	this.loadImage2 = function (image) {
+	// bugbug todo break this up into setupinputimage and drawinputimage
+	this.drawInputImage = function () {
 
-		//convert the image to a square image via the temporary 2d canvas 
-		var canvas = document.getElementById("inputcanvas");
-		var ctx = canvas.getContext("2d");
-		
-		var ll = document.getElementById("inputcanvas").height;
+		var ll = this.canvas.height;	// Canvas is square, so only need to get one side
 
 		var x = 0;
 		var y = 0;
 		var xx = ll;
 		var yy = ll;
 
-		ctx.clearRect(0, 0, ll, ll);
+		this.ctx.clearRect(0, 0, ll, ll);
 
-		if (image.width < image.height) {
+		if (this.inputImage.width < this.inputImage.height) {
 			// change x, xx
-			xx = image.width / image.height * ll;
+			xx = this.inputImage.width / this.inputImage.height * ll;
 			x = (ll - xx) / 2;
 		}
-		if (image.width > image.height) {
+		if (this.inputImage.width > this.inputImage.height) {
 			// change y, yy
-			yy = image.height / image.width * ll;
+			yy = this.inputImage.height / this.inputImage.width * ll;
 			y = (ll - yy) / 2;
 		}
 
-		ctx.drawImage(image, 0, 0, image.width, image.height, x, y, xx, yy);
+		this.ctx.drawImage(this.inputImage, 0, 0, this.inputImage.width, this.inputImage.height, x, y, xx, yy);
+
+	}
+
+	this.loadImage2 = function (image) {
+
+		//convert the image to a square image via the temporary 2d canvas 
+
+		this.drawInputImage();
 
 		var gl = this.gl;
 		var texture = gl.createTexture();
@@ -230,7 +237,7 @@ var renderer = new function() {
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 
 		// Upload the image into the texture.
-		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, canvas);
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.canvas);
 
 		//bugbug split this into seperate files / interfaces
 		this.render();
@@ -242,7 +249,7 @@ var renderer = new function() {
 	this.renderHeight = 1;
 
 
-	this.updateCorners = function(corners) {
+	this.updateCorners = function (corners) {
 		//console.log("! " + corners.topLeft.x + " " + corners.topLeft.y + " " + corners.topRight.x + " " + corners.topRight.y);
 
 		this.gl.uniform2f(this.pictureprogram.topLeft, corners.topLeft.x, corners.topLeft.y);
@@ -251,33 +258,40 @@ var renderer = new function() {
 		this.gl.uniform2f(this.pictureprogram.bottomLeft, corners.bottomLeft.x, corners.bottomLeft.y);
 		this.render();
 
-
-		
-		
-	
-		// take the average of the two sides
+		// take the average of the two sides - this helps set the right aspect ratio for the output image
 		this.renderWidth = (corners.topLeft.distanceFrom(corners.topRight) + corners.bottomLeft.distanceFrom(corners.bottomRight)) / 2;
 		this.renderHeight = (corners.topLeft.distanceFrom(corners.bottomLeft) + corners.topRight.distanceFrom(corners.bottomRight)) / 2;
+
+		this.drawInputImage();
+		this.ctx.beginPath();
+		this.ctx.strokeStyle = "rgb(100, 0, 0)";
+		this.ctx.setLineDash([5, 5]);
+		this.ctx.moveTo(corners.topLeft.x * this.canvas.width, (1 - corners.topLeft.y) * this.canvas.height);
+		this.ctx.lineTo(corners.topRight.x * this.canvas.width, (1 - corners.topRight.y) * this.canvas.height);
+		this.ctx.lineTo(corners.bottomRight.x * this.canvas.width, (1 - corners.bottomRight.y) * this.canvas.height);
+		this.ctx.lineTo(corners.bottomLeft.x * this.canvas.width, (1 - corners.bottomLeft.y) * this.canvas.height);
+		this.ctx.lineTo(corners.topLeft.x * this.canvas.width, (1 - corners.topLeft.y) * this.canvas.height);
+		this.ctx.stroke();
 
 
 	}.bind(this)
 
 	this.render = function () {
-   
+
 		var gl = this.gl;
-		
-		gl.clearColor(1.0, 0.0, 1.0, 0.5);
+
+		gl.clearColor(0.0, 0.0, 0.0, 0.0);
 		gl.clear(this.gl.COLOR_BUFFER_BIT);
 
 		var program;
-        
-        // TODO drawElements would be more efficient here
+
+		// TODO drawElements would be more efficient here
 
 
 		program = this.pictureprogram;
 
 		// Set the viewport we shoudl draw into
-		
+
 		var middleX = gl.viewportWidth / 2;
 		var middleY = gl.viewportHeight / 2;
 		var offsetX = (this.renderWidth * gl.viewportWidth);
@@ -286,7 +300,7 @@ var renderer = new function() {
 		gl.viewport(middleX - offsetX / 2, middleY - offsetY / 2, offsetX, offsetY);
 
 		gl.bindBuffer(gl.ARRAY_BUFFER, this.texCoordBuffer);
-		
+
 		gl.useProgram(this.pictureprogram);
 
 		gl.vertexAttribPointer(this.texCoordLocation, 2, gl.FLOAT, false, 0, 0);
@@ -300,7 +314,7 @@ var renderer = new function() {
 
 
 	}
-	
+
 
 
 
@@ -319,7 +333,7 @@ function main() {
 	cornerUI.init(document.getElementById('inputcanvas'), renderer.updateCorners);
 
 
-    //init2();
+	//init2();
 
 }
 
@@ -336,14 +350,14 @@ function setVec2(id, a, b) {
 
 // Adds a string to the log in the web page
 function log(result) {
-    var resultDiv = document.getElementById("log");
-    resultDiv.innerHTML += result + "<br />";
+	var resultDiv = document.getElementById("log");
+	resultDiv.innerHTML += result + "<br />";
 }
 
 // Adds a string to the log in the web page; overwrites everything in the log with the new string
 function logi(result) {
-    var resultDiv = document.getElementById('log');
-    resultDiv.innerHTML = result;
+	var resultDiv = document.getElementById('log');
+	resultDiv.innerHTML = result;
 }
 
 
@@ -351,59 +365,58 @@ function logi(result) {
 // [in] WebGL context
 // [in] id of script element with the shader to load
 function getShader(gl, id) {
-    var shaderScript = document.getElementById(id);
-    if (!shaderScript) {
-        return null;
-    }
+	var shaderScript = document.getElementById(id);
+	if (!shaderScript) {
+		return null;
+	}
 
-    var str = "";
-    var k = shaderScript.firstChild;
-    while (k) {
-        if (k.nodeType == 3)
-            str += k.textContent;
-        k = k.nextSibling;
-    }
+	var str = "";
+	var k = shaderScript.firstChild;
+	while (k) {
+		if (k.nodeType == 3)
+			str += k.textContent;
+		k = k.nextSibling;
+	}
 
-    var shader;
-    if (shaderScript.type == "x-shader/x-fragment") {
-        shader = gl.createShader(gl.FRAGMENT_SHADER);
-    } else if (shaderScript.type == "x-shader/x-vertex") {
-        shader = gl.createShader(gl.VERTEX_SHADER);
-    } else {
-        return null;
-    }
+	var shader;
+	if (shaderScript.type == "x-shader/x-fragment") {
+		shader = gl.createShader(gl.FRAGMENT_SHADER);
+	} else if (shaderScript.type == "x-shader/x-vertex") {
+		shader = gl.createShader(gl.VERTEX_SHADER);
+	} else {
+		return null;
+	}
 
-    gl.shaderSource(shader, str);
-    gl.compileShader(shader);
+	gl.shaderSource(shader, str);
+	gl.compileShader(shader);
 
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        alert(gl.getShaderInfoLog(shader));
-        return null;
-    }
+	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+		alert(gl.getShaderInfoLog(shader));
+		return null;
+	}
 
-    return shader;
+	return shader;
 }
 
 
-function loadProgram(gl, vertexshader, fragmentshader)
-{
-    var program = gl.createProgram();
-    gl.attachShader(program, vertexshader);
-    gl.attachShader(program, fragmentshader);
+function loadProgram(gl, vertexshader, fragmentshader) {
+	var program = gl.createProgram();
+	gl.attachShader(program, vertexshader);
+	gl.attachShader(program, fragmentshader);
 
-    gl.linkProgram(program);
+	gl.linkProgram(program);
 
-    // Check the link status
-    var linked = gl.getProgramParameter(program, gl.LINK_STATUS);
-    if (!linked) {
-        // something went wrong with the link
-        lastError = gl.getProgramInfoLog(program);
-        alert("Error in program linking:" + lastError);
+	// Check the link status
+	var linked = gl.getProgramParameter(program, gl.LINK_STATUS);
+	if (!linked) {
+		// something went wrong with the link
+		lastError = gl.getProgramInfoLog(program);
+		alert("Error in program linking:" + lastError);
 
-        gl.deleteProgram(program);
-        return;
-    }
-    return program;
+		gl.deleteProgram(program);
+		return;
+	}
+	return program;
 };
 
 
