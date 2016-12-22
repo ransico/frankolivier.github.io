7a2578ac1c110b519dd9da534204ea3e856286ee
frankolivier <franko@microsoft.com>
2016-12-22 12:41:44 -0800

    Added global font

--- 360video/js.js
+++ 360video/js.js
@@ -18,7 +18,7 @@ var frameData;
 
 var doVideo = true;
 
-var frameCounter = 0; 
+var frameCounter = 0;
 
 var fpsData = "";
 
@@ -258,10 +258,9 @@ function init() {
 
 
 
-	
 
-	if (true === doVideo)
-	{
+
+	if (true === doVideo) {
 		videoElement = document.getElementById('sourcevideo');
 		gl.bindTexture(gl.TEXTURE_2D, videoTexture);
 		//gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
@@ -274,11 +273,9 @@ function init() {
 	}
 
 
-	if ( vrDisplay == null)
-	{
+	if (vrDisplay == null) {
 	}
-	else
-	{
+	else {
 		frameData = new VRFrameData();
 	}
 
@@ -334,29 +331,26 @@ function onVRFrame() {
 
 	var t0 = performance.now();
 
-	if (vrDisplay==null)
-	{
+	if (vrDisplay == null) {
 		window.requestAnimationFrame(onVRFrame);
 	}
-	else
-	{
+	else {
 		vrDisplay.requestAnimationFrame(onVRFrame);
 	}
 
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
 
-   var uploaded = false;
+	var uploaded = false;
 
-	if (true === doVideo)
-	{	
-		var thisVideoUploadTime = t0; 
+	if (true === doVideo) {
+		var thisVideoUploadTime = t0;
 
 		if (thisVideoUploadTime - previousVideoUploadTime > 33.33333) // aka 30fps (1000ms / 30 fps)
 		{
 			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, videoElement);
 			//gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, 1024, 1024, gl.RGBA, gl.UNSIGNED_BYTE, videoElement);
-			
+
 			//void texImage2D(enum target, int level, enum internalformat, enum format, enum type, Object object)
 			//void texSubImage2D(enum target, int level, int xoffset, int yoffset, long width, long height, enum format, enum type, Object pixels)
 
@@ -365,16 +359,15 @@ function onVRFrame() {
 			//fpsData += ('frame upload<br />');
 
 		}
-		
+
 	}
 
-    var leftPM;
+	var leftPM;
 	var leftVM;
 	var rightPM;
 	var rightVM;
 
-	if (vrDisplay == null)
-	{
+	if (vrDisplay == null) {
 		/*
 		leftPM = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
 		leftVM = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
@@ -382,16 +375,15 @@ function onVRFrame() {
 		rightVM = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
 		*/
 
-		leftPM = [ 0.757, 0.000, 0.000, 0.000, 0.000, 0.681, 0.000, 0.000,-0.056,-0.002,-1.000,-1.000, 0.000, 0.000,-0.020, 0.000 ];
-		leftVM = [ 0.938,-0.191,-0.288, 0.000, 0.215, 0.975, 0.052, 0.000, 0.271,-0.111, 0.956, 0.000, 0.667,-0.028,-0.361, 1.000 ];
-		rightPM = [ 0.757, 0.000, 0.000, 0.000, 0.000, 0.682, 0.000, 0.000, 0.056, 0.005,-1.000,-1.000, 0.000, 0.000,-0.020, 0.000 ];
-		rightVM = [ 0.938,-0.191,-0.288, 0.000, 0.215, 0.975, 0.052, 0.000, 0.271,-0.111, 0.956, 0.000, 0.603,-0.028,-0.361, 1.000 ];
+		leftPM = [0.757, 0.000, 0.000, 0.000, 0.000, 0.681, 0.000, 0.000, -0.056, -0.002, -1.000, -1.000, 0.000, 0.000, -0.020, 0.000];
+		leftVM = [0.938, -0.191, -0.288, 0.000, 0.215, 0.975, 0.052, 0.000, 0.271, -0.111, 0.956, 0.000, 0.667, -0.028, -0.361, 1.000];
+		rightPM = [0.757, 0.000, 0.000, 0.000, 0.000, 0.682, 0.000, 0.000, 0.056, 0.005, -1.000, -1.000, 0.000, 0.000, -0.020, 0.000];
+		rightVM = [0.938, -0.191, -0.288, 0.000, 0.215, 0.975, 0.052, 0.000, 0.271, -0.111, 0.956, 0.000, 0.603, -0.028, -0.361, 1.000];
 
 	}
-	else
-	{
+	else {
 		vrDisplay.getFrameData(frameData);
-			
+
 
 
 		leftPM = frameData.leftProjectionMatrix;
@@ -401,38 +393,38 @@ function onVRFrame() {
 	}
 
 	// Center
-	leftVM[12] = 0; 
-	leftVM[13] = 0; 
+	leftVM[12] = 0;
+	leftVM[13] = 0;
 	leftVM[14] = 0;
 
-	rightVM[12] = 0; 
-	rightVM[13] = 0; 
+	rightVM[12] = 0;
+	rightVM[13] = 0;
 	rightVM[14] = 0;
 
-/*
-	var log = document.getElementById('log');
-
-    var d = 2;
-
-	log.innerHTML = (frameData.leftViewMatrix[0].toFixed(d) + " " 
-	              + frameData.leftViewMatrix[1].toFixed(d) + " " 
-				  + frameData.leftViewMatrix[2].toFixed(d) + " "
-				  + frameData.leftViewMatrix[3].toFixed(d)) +
-				  + " | "
-				  + (frameData.leftViewMatrix[4].toFixed(d) + " " 
-	              + frameData.leftViewMatrix[5].toFixed(d) + " " 
-				  + frameData.leftViewMatrix[6].toFixed(d) + " "
-				  + frameData.leftViewMatrix[7].toFixed(d)) +
-				  + " | "
-				  + frameData.leftViewMatrix[8].toFixed(d) + " " 
-	              + frameData.leftViewMatrix[9].toFixed(d) + " " 
-				  + frameData.leftViewMatrix[10].toFixed(d) + " "
-				  + frameData.leftViewMatrix[11].toFixed(d) +
-				  + " | "
-				  + frameData.leftViewMatrix[12].toFixed(d) + " " 
-	              + frameData.leftViewMatrix[13].toFixed(d) + " " 
-				  + frameData.leftViewMatrix[14].toFixed(d) + " "
-				  + frameData.leftViewMatrix[15].toFixed(d); */
+	/*
+		var log = document.getElementById('log');
+	
+		var d = 2;
+	
+		log.innerHTML = (frameData.leftViewMatrix[0].toFixed(d) + " " 
+					  + frameData.leftViewMatrix[1].toFixed(d) + " " 
+					  + frameData.leftViewMatrix[2].toFixed(d) + " "
+					  + frameData.leftViewMatrix[3].toFixed(d)) +
+					  + " | "
+					  + (frameData.leftViewMatrix[4].toFixed(d) + " " 
+					  + frameData.leftViewMatrix[5].toFixed(d) + " " 
+					  + frameData.leftViewMatrix[6].toFixed(d) + " "
+					  + frameData.leftViewMatrix[7].toFixed(d)) +
+					  + " | "
+					  + frameData.leftViewMatrix[8].toFixed(d) + " " 
+					  + frameData.leftViewMatrix[9].toFixed(d) + " " 
+					  + frameData.leftViewMatrix[10].toFixed(d) + " "
+					  + frameData.leftViewMatrix[11].toFixed(d) +
+					  + " | "
+					  + frameData.leftViewMatrix[12].toFixed(d) + " " 
+					  + frameData.leftViewMatrix[13].toFixed(d) + " " 
+					  + frameData.leftViewMatrix[14].toFixed(d) + " "
+					  + frameData.leftViewMatrix[15].toFixed(d); */
 
 	/*
     if (navigator.getVRDisplays)
@@ -476,8 +468,7 @@ function onVRFrame() {
 	// Indicate that we are ready to present the rendered frame to the VRDisplay
 	//bugbug vrDisplay.submitFrame();
 	//window.requestAnimationFrame(onFrame);
-	if (vrDisplay != null)
-	{
+	if (vrDisplay != null) {
 		vrDisplay.submitFrame();
 	}
 
@@ -486,19 +477,18 @@ function onVRFrame() {
 	var t = (t1 - t0).toFixed(4);
 
 
-	if (uploaded == true)
-	{
-		fpsData += (t + '*<br />');
-	}
-	else
-	{
-		fpsData += (t + '<br />');
-	}
 
-    frameCounter++;
-	if (frameCounter == 1000)
-	{
-		log(fpsData);
+	frameCounter++;
+	if (frameCounter <= 1000) {
+
+		if (uploaded == true) {
+			fpsData += (t + '*<br />');
+		}
+		else {
+			fpsData += (t + '<br />');
+		}
+
+		if (frameCounter == 1000) log(fpsData);
 	}
 
 }
@@ -523,31 +513,30 @@ function enterVR() {
 
 	canvas = document.getElementById('webglcanvas');
 	//gl = canvas.getContext('experimental-webgl');
-	gl = canvas.getContext("experimental-webgl", { 	alpha:false, 
-													preserveDrawingBuffer: false,
-													depth: false,
-													stencil: false,
-													antialias: false,
-													premultipliedAlpha: true
-													 });
+	gl = canvas.getContext("experimental-webgl", {
+		alpha: false,
+		preserveDrawingBuffer: false,
+		depth: false,
+		stencil: false,
+		antialias: false,
+		premultipliedAlpha: true
+	});
 
 
 
 
-    if (vrDisplay==null)
-	{
-				init();
-				window.requestAnimationFrame(onVRFrame);
+	if (vrDisplay == null) {
+		init();
+		window.requestAnimationFrame(onVRFrame);
 	}
-	else
-	{
-			vrDisplay.requestPresent([{ source: canvas }]).then(function () {
-				init();
-				vrDisplay.requestAnimationFrame(onVRFrame);
-			});
+	else {
+		vrDisplay.requestPresent([{ source: canvas }]).then(function () {
+			init();
+			vrDisplay.requestAnimationFrame(onVRFrame);
+		});
 	}
 
-	
+
 	// Begin presentation (must be called within a user gesture)
 
 }
